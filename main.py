ad535e0fd5da2f8e319d26b526540c4bc40dfe20
bob <bob@pop-os.localdomain>
2020-04-12 19:49:34 +1200

    basic exsample

---
+++ main.py
@@ -0,0 +1,24 @@
+from curio import sleep
+from bricknil import attach, start
+from bricknil.hub import PoweredUpHub
+from bricknil.sensor import TrainMotor
+import logging
+
+
+@attach(TrainMotor, name='motor')
+class Train(PoweredUpHub):
+
+    async def run(self):
+        for i in range(2):  # Repeat this control two times
+            await self.motor.ramp_speed(80,5000) # Ramp speed to 80 over 5 seconds
+            await sleep(6)
+            await self.motor.ramp_speed(0,1000)  # Brake to 0 over 1 second
+            await sleep(2)
+
+async def system():
+    train = Train('cargo_train')
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start(system)
