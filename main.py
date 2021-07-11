87737bb48ac55ab90e425e0ff80a8d60fad66c57
Rowan Self <rowan.self@gmail.com>
2021-07-11 22:13:52 +1200

    use pylbst package

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from curio import sleep
-from bricknil import attach, start
-from bricknil.hub import PoweredUpHub
-from bricknil.sensor import TrainMotor
+# from curio import sleep
+# from bricknil import attach, start
+# from bricknil.hub import PoweredUpHub
+# from bricknil.sensor import TrainMotor
 import logging
+from pylgbst import *
+from pylgbst.hub import MoveHub
 
+# @attach(TrainMotor, name='motor')
+# class Train(PoweredUpHub):
 
-@attach(TrainMotor, name='motor')
-class Train(PoweredUpHub):
+#     async def run(self):
+#         for i in range(2):  # Repeat this control two times
+#             await self.motor.ramp_speed(80,5000) # Ramp speed to 80 over 5 seconds
+#             await sleep(6)
+#             await self.motor.ramp_speed(0,1000)  # Brake to 0 over 1 second
+#             await sleep(2)
 
-    async def run(self):
-        for i in range(2):  # Repeat this control two times
-            await self.motor.ramp_speed(80,5000) # Ramp speed to 80 over 5 seconds
-            await sleep(6)
-            await self.motor.ramp_speed(0,1000)  # Brake to 0 over 1 second
-            await sleep(2)
-
-async def system():
-    train = Train('cargo_train')
+# async def system():
+#     train = Train('cargo_train')
     
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    start(system)
+    conn=get_connection_auto()
+    try:
+        movehub = MoveHub(conn)
+        print("dsffds")
+    finally:  
+        conn.disconnect()      
+    #start(system)
